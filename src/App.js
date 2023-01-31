3a6a8fe05dab1e78bb08a13ad958687e38a454d0
Sanchit Ingale <sanchitingale09@gmail.com>
2023-01-31 07:58:48 +0530

    login register done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Register from "./components/Register";
+import Login from "./components/Login";
+import "./style.scss"
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
